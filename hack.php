35960527ea4026c3ce5d9004f48087fe633e1cf0
alex <aleksandar.kanchev@sensika.com>
2021-01-14 23:41:55 +0200

    Optimize Extractor functions

--- hack.php
+++ hack.php
@@ -2,9 +2,10 @@
 
 require_once 'bootstrap.php';
 
-Extractor::extractInstrumentsMetadata();
-Extractor::extractInstrumentsInfo();
-Extractor::extractInstrumentsDataTickerData();
-Extractor::updateInstrumentsClosingPrices();
-Extractor::updateInstrumentsRates();
+//Extractor::extractInstrumentsMetadata();
+//Extractor::extractInstrumentsInfo();
+//Extractor::extractInstrumentsDataTickerData();
+//Extractor::updateInstrumentsClosingPrices();
+//Extractor::updateInstrumentsDataFilters();
+//Extractor::extractInvested();
 Extractor::exportToJSON();
